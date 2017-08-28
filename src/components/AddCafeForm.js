6a1828be2fbc8ed4514ce3e33f8a07b3e6177470
Thomas Wija <thomas.wija@3ev.com>
2017-08-28 13:38:58 +0100

    Added address form

--- src/components/AddCafeForm.js
+++ src/components/AddCafeForm.js
@@ -8,6 +8,7 @@ class AddCafeForm extends Component {
     constructor() {
         super();
         this.addCafe = this.addCafe.bind(this);
+        this.createCafe = this.createCafe.bind(this);
     }
 
     state = {
@@ -32,9 +33,21 @@ class AddCafeForm extends Component {
     createCafe(event) {
         event.preventDefault();
         const cafe = {
-          name: this.name.value,
-          description: this.description.value
+            name: this.refs.name.value,
+            description: this.refs.description.value,
+            address1: this.refs.address1.value,
+            address2: this.refs.address2.value,
+            city: this.refs.city.value,
+            postcode: this.refs.postcode.value
         }
+
+        this.refs.name.value = "";
+        this.refs.description.value = "";
+        this.refs.address1.value = "";
+        this.refs.address2.value = "";
+        this.refs.city.value = "";
+        this.refs.postcode.value = "";
+
         this.addCafe(cafe);
     }
 
@@ -43,13 +56,50 @@ class AddCafeForm extends Component {
             <div>
                 <Header />
                 <div className="app text-center">
-                    <h1 className="heading">Add Your Cafe</h1>
+                    <div className="container container--column">
+                        <h1 className="heading">Add Your Cafe</h1>
+                        <form ref={(input) => { this.cafeForm = input; }} className="cafe-edit" onSubmit={(e) => this.createCafe(e)}>
+                            <div className="form-group form-group--row">
+                                <div className="form-control">
+                                    <label>Cafe Name</label>
+                                    <input ref="name" type="text" placeholder="Cafe Name" />
+                                </div>
 
-                    <form ref={(input) => this.cafeForm = input} className="cafe-edit" onSubmit={(e) => this.createCafe(e)}>
-                        <input ref={(input) => this.name = input} type="text" placeholder="Cafe Name" />
-                        <input ref={(input) => this.description = input} type="text" placeholder="Cafe Description" />
-                        <button type="submit">+ Add Cafe</button>
-                    </form>
+                                <div className="form-control">
+                                    <label>Short Description</label>
+                                    <input ref="description" type="text" placeholder="Short Description" />
+                                </div>
+                            </div>
+
+                            <hr />
+
+                            <div className="form-group form-group--column">
+                                <div className="form-control">
+                                    <label>Address Line 1*</label>
+                                    <input ref="address1" type="text" placeholder="Address 1" />
+                                </div>
+
+                                <div className="form-control">
+                                    <label>Address Line 2</label>
+                                    <input ref="address2" type="text" placeholder="Address 2" />
+                                </div>
+
+                                <div className="form-control">
+                                    <label>City*</label>
+                                    <input ref="city" type="text" placeholder="City" />
+                                </div>
+
+                                <div className="form-control">
+                                    <label>Postcode*</label>
+                                    <input ref="postcode" type="text" placeholder="Postcode" />
+                                </div>
+                            </div>
+
+                            <hr />
+
+                            <button type="submit">+ Add Cafe</button>
+                        </form>
+                    </div>
                 </div>
             </div>
         );
